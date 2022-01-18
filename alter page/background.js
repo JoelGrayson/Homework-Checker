de4769ff5087298ae9c52c836f61fe2f58064899
JoelGrayson <joel@joelgrayson.com>
2022-01-18 06:06:32 -0500

    v3.2: Show/hide beta features

--- alter page/background.js
+++ alter page/background.js
@@ -2,15 +2,21 @@ chrome.runtime.onInstalled.addListener(()=>{
     chrome.storage.sync.set({
         settings: {
             showCheckmarks: 'always', //'onHover' | 'always'
+            betaEnabled: false //shows beta features
         },
         checkedTasks: {} //object with keys of courseNames and values of list of strings of assignments already checked, all other assignments remain unchecked
     });
 });
 
 /*
-checkedTasks
+chrome.storage:
 {
-    '$class': ['checked assignment 1', 'assignment 2'],
-    'english': ['read macbeth', 'study for grammar quiz']
+    settings: {
+        showCheckmarks: 'onHover' | 'always'
+    },
+    checkedTasks: {
+        '$class': ['checked assignment 1', 'assignment 2'],
+        'english': ['read macbeth', 'study for grammar quiz']
+    }
 }
 */
--- popup/main.js
+++ popup/main.js
@@ -1,32 +1,64 @@
 const onlyShowOnHoverEl=document.getElementById('onlyShowOnHover');
+const betaEnabledEl=document.getElementById('betaEnabled');
 
 (async ()=>{ //Check the click if true or not
     let data=await chrome.storage.sync.get();
-    if (data.settings.showCheckmarks==='always')
-        onlyShowOnHoverEl.checked=false;
-    else if (data.settings.showCheckmarks==='onHover')
-        onlyShowOnHoverEl.checked=true;
-    else
-        throw new Error('Unknown chrome.storage.settings.showCheckmarks value:', data.settings.showCheckmarks);
+    switch (data.settings.showCheckmarks) {
+        case 'always':
+            onlyShowOnHoverEl.checked=false;
+            break;
+        case 'hover':
+            onlyShowOnHoverEl.checked=true;
+            break;
+        default:
+            throw new Error('Unknown chrome.storage.settings.showCheckmarks value:', data.settings.showCheckmarks);
+    }
+
+    if (data.settings.betaEnabled) { //if betaEnabled
+        betaEnabledEl.checked=true;
+        enableBeta();        
+    } else {
+        betaEnabledEl.checked=false;
+    }
 })();
 
-onlyShowOnHoverEl.addEventListener('change', async e=>{
+onlyShowOnHoverEl.addEventListener('change', async ()=>{
     let data=await chrome.storage.sync.get(); //get current settings
     data.settings.showCheckmarks=(onlyShowOnHoverEl.checked?'onHover':'always');
     chrome.storage.sync.set(data);
 
-    sendMessage({run: 'reload'});
+    sendMessage({run: 'reload'}); //reloads when checking
 });
 
+let betaEnabledEventListeners=[];
 
-//check all assignments
-document.getElementById('checkAll').addEventListener('click', ()=>{
-    sendMessage({run: 'check all assignments'});
+function enableBeta() {
+    // Reveal elements
+    let betaEls=document.getElementsByClassName('beta');
+    for (let i=0; i<betaEls.length; i++)
+        betaEls[i].style.display='block';
+    
+    // Add event listeners to beta elements
+    //check all assignments
+    document.getElementById('checkAll').addEventListener('click', ()=>{
+        sendMessage({run: 'check all assignments'});
+    })
+    //check all assignments before today
+    document.getElementById('checkAllBeforeToday').addEventListener('click', ()=>{
+        sendMessage({run: 'check all assignments before today'});
+    })
+}
+
+betaEnabledEl.addEventListener('change', async ()=>{
+    let currentStatus=betaEnabledEl.checked;
+    if (currentStatus)
+        enableBeta();
+    let data=await chrome.storage.sync.get();
+    data.settings.betaEnabled=currentStatus;
+    chrome.storage.sync.set(data);
+    location.reload(); //reload for new DOM render
 })
-//check all assignments before today
-document.getElementById('checkAllBeforeToday').addEventListener('click', ()=>{
-    sendMessage({run: 'check all assignments before today'});
-})
+
 
 //Reset Button
 document.getElementById('resetBtn').addEventListener('click', async e=>{
