f7572e3c154167f1e687551df348aa2ca2403d37
JoelGrayson <joel@joelgrayson.com>
2022-02-06 14:32:13 -0500

    Background.js patch

--- alter page/background.js
+++ alter page/background.js
@@ -6,8 +6,14 @@ chrome.runtime.onInstalled.addListener(()=>{
             overdueCollapsed: false, //when true, overdue section is collapsed
         },
         checkedTasks: {} //object with keys of courseNames and values of list of strings of assignments already checked, all other assignments remain unchecked
+        /*
+        Example:
+            '$classWithNoSpaces': ['checked assignment 1', 'assignment 2'], //no spaces because removeSpaces()
+            'englishII': ['read macbeth', 'study for grammar quiz']
+        } */
     });
 });
+
 chrome.runtime.onUpdateAvailable.addListener(details=>{ //update extension and let know updated
     console.log({details});
     chrome.tabs.create({ //create new tab
@@ -15,22 +21,3 @@ chrome.runtime.onUpdateAvailable.addListener(details=>{ //update extension and l
     });
     chrome.runtime.reload();
 });
-
-setInterval(()=>{
-    chrome.storage.sync.get('settings', settings=>{
-        console.log(settings);
-    });
-}, 1000);
-
-/*
-chrome.storage:
-{
-    settings: {
-        showCheckmarks: 'onHover' | 'always'
-    },
-    checkedTasks: {
-        '$classWithNoSpaces': ['checked assignment 1', 'assignment 2'], //no spaces because removeSpaces()
-        'englishII': ['read macbeth', 'study for grammar quiz']
-    }
-}
-*/
