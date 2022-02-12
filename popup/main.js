b7554494d6db69d15a39fad2813990a274fbf9e0
JoelGrayson <joel@joelgrayson.com>
2022-02-12 16:17:00 -0500

    Popup: Remove beta features because not necessary

--- popup/main.js
+++ popup/main.js
@@ -1,3 +1,4 @@
+//Toggles Require Elements to be updated when chrome storage data arrives, so elemnents below
 const onlyShowOnHoverEl=document.getElementById('onlyShowOnHover');
 const overdueCollapsed=document.getElementById('overdueCollapsed');
 const showCheckmarksValues={ //enum of valid values
@@ -5,8 +6,7 @@ const showCheckmarksValues={ //enum of valid values
     onHover: 'onHover'
 };
 
-const betaEnabledEl=document.getElementById('betaEnabled');
-
+//*|| Import data from chrome storage
 (async ()=>{ //Assigns switches the proper checked/unchecked values (fetches from chrome storage and reflects changes on switches on/off)
     let data=await chrome.storage.sync.get();
     switch (data.settings.showCheckmarks) {
@@ -23,14 +23,9 @@ const betaEnabledEl=document.getElementById('betaEnabled');
     overdueCollapsed.checked=data.settings.overdueCollapsed;
     console.log(data.settings.overdueCollapsed)
 
-    if (data.settings.betaEnabled) { //if betaEnabled
-        betaEnabledEl.checked=true;
-        enableBeta();        
-    } else {
-        betaEnabledEl.checked=false;
-    }
 })();
 
+//*|| Toggle EventListeners
 onlyShowOnHoverEl.addEventListener('change', async ()=>{
     let data=await chrome.storage.sync.get(); //get current settings
     data.settings.showCheckmarks=(onlyShowOnHoverEl.checked?showCheckmarksValues.onHover:showCheckmarksValues.always);
@@ -47,39 +42,11 @@ overdueCollapsed.addEventListener('change', async ()=>{
     sendMessage({run: 'reload'}); //reloads when checking
 });
 
-let betaEnabledEventListeners=[];
-
-function enableBeta() {
-    // Reveal elements
-    let betaEls=document.getElementsByClassName('beta');
-    for (let i=0; i<betaEls.length; i++)
-        betaEls[i].style.display='block';
-    
-    // Add event listeners to beta elements
-    //check all assignments
-    document.getElementById('checkAll').addEventListener('click', ()=>{
-        sendMessage({run: 'check all assignments'});
-    })
-    //check all assignments before today
-    document.getElementById('checkAllBeforeToday').addEventListener('click', ()=>{
-        sendMessage({run: 'check all assignments before today'});
-    })
-}
-
-betaEnabledEl.addEventListener('change', async ()=>{
-    let currentStatus=betaEnabledEl.checked;
-    if (currentStatus)
-        enableBeta();
-    let data=await chrome.storage.sync.get();
-    data.settings.betaEnabled=currentStatus;
-    chrome.storage.sync.set(data);
-    if (!currentStatus) {
-        setTimeout(()=>{ //after some time for chrome.sync to work
-            location.reload(); //reload for new DOM render
-        }, 500);
-    }
-})
-
+//*|| Button EventListeners
+//Check all assignments
+document.getElementById('checkAll').addEventListener('click', ()=>{
+    sendMessage({run: 'check all assignments'});
+});
 
 //Uncheck all (reset) Button
 document.getElementById('resetBtn').addEventListener('click', async e=>{
@@ -89,8 +56,14 @@ document.getElementById('resetBtn').addEventListener('click', async e=>{
     sendMessage({run: 'reload'}); //reload page for changes to show
 });
 
+//check all assignments before today
+document.getElementById('checkAllBeforeToday').addEventListener('click', ()=>{
+    sendMessage({run: 'check all assignments before today'});
+});
 
-//Functionality
+
+
+//*|| Functionality
 function sendMessage(msg) {
     chrome.tabs.query({currentWindow: true, active: true}, (tabs)=>{
         let activeTab=tabs[0];
